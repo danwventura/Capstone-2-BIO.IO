ecd8d5cdc649b5858b251e5b84572e0bb1a9668c
Dan <danwventura@gmail.com>
2016-12-07 21:21:55 -0600

    User Model

---
+++ Bio.io/Bio.io/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bio.io.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Device> Devices { get; set; }
+        public List<Route> Routes { get; set; }
+        public DateTime MemberSince { get; set; }
+    }
+}
