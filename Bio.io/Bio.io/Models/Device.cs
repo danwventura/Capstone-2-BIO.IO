99a372fda0a25582ecc81c010a20929016a6a8f6
Dan <danwventura@gmail.com>
2016-12-07 21:07:37 -0600

    Device Model

---
+++ Bio.io/Bio.io/Models/Device.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bio.io.Models
+{
+    public class Device
+    {
+        [Key]
+        public int DeviceID { get; set; }
+        public string Name { get; set; }
+        public DateTime LastTransmit { get; set; }
+    }
+}
