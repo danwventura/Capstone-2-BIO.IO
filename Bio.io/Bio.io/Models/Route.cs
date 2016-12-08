2ba4681629fe487f3fe6fb7325a7299af197ecd5
Dan <danwventura@gmail.com>
2016-12-07 21:16:58 -0600

    Route Model

---
+++ Bio.io/Bio.io/Models/Route.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bio.io.Models
+{
+    public class Route
+    {   [Key]
+        public int RouteID { get; set; }
+        public List<DataPoint> Coordinates { get; set; }
+        public List<Images> Snapshots { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
