d8c5529f2a14d5eb883f71efd07481fe3a27e206
Dan <danwventura@gmail.com>
2016-12-07 21:00:48 -0600

    initial project setup with gitignore

---
+++ Bio.io/Bio.io/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bio.io.MvcApplication" Language="C#" %>
---
+++ Bio.io/Bio.io/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bio.io.Startup))]
+namespace Bio.io
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
