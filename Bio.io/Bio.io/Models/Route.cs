f63722d4d6fc8c9818edd7932a0ef17b3ed621f2
Dan <danwventura@gmail.com>
2016-12-08 19:20:31 -0600

    added one more virtual list

--- Bio.io/Bio.io/Models/Route.cs
+++ Bio.io/Bio.io/Models/Route.cs
@@ -9,7 +9,7 @@ namespace Bio.io.Models
     public class Route
     {   [Key]
         public int RouteID { get; set; }
-        public List<DataPoint> Coordinates { get; set; }
+        public virtual List<DataPoint> Coordinates { get; set; }
         public virtual List<Image> Snapshots { get; set; }
         public DateTime Created { get; set; }
     }
